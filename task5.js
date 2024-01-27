3a11bda4ff743902e542b9c9ef7638ddfd85d195
maabfw <maabfw@proton.me>
2024-01-27 17:03:04 +0600

    task5.js done

--- task5.js
+++ task5.js
@@ -2,14 +2,15 @@ let num1 = 100;
 let num2 = 50;
 let result
 
-// if (num1 > num2) {
-//     result = (num1 * 2)
-//     console.log(result)
+if (num1 > num2) {
+    result = (num1 * 2)
+    console.log(result)
 
-// } 
-// else {
-//     result = (num1 + num2)
-//     console.log(result)
-// }
+} 
+else {
+    result = (num1 + num2)
+    console.log(result)
+}
 
-num1>num2 ? result= num1*2 : result=num1+num2
+result =  num1>num2 ? num1*2 : num1+num2
+console.log(result)
