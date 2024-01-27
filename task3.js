9de632c7313d309cf20077164a47f2103ca70ec0
maabfw <maabfw@proton.me>
2024-01-27 11:59:43 +0600

    task 3 done

---
+++ task3.js
@@ -0,0 +1,16 @@
+let score = 89;
+if(90 <= score){
+    console.log('A')
+}
+else if(80 <= score){
+    console.log('B')
+}
+else if(70 <= score){
+    console.log('C')
+}
+else if(60 <= score){
+    console.log('D')
+}
+else {
+    console.log('F')
+}
