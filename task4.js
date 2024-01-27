98030f78913dc549d839b908f255889d30aef82b
maabfw <maabfw@proton.me>
2024-01-27 12:09:32 +0600

    task 4 done

---
+++ task4.js
@@ -0,0 +1,20 @@
+let myScore = 81;
+let friendScore = 33;
+
+if (myScore>80){
+    if(friendScore>80){
+        console.log('go for a lunch')
+    }
+    else if (friendScore >=60){
+        console.log('good luck next time')
+    }
+    else if(friendScore>=40){
+        console.log('message unseen')
+    }
+    else{
+        console.log('block')
+    }
+}
+else{
+    console.log('im sad')
+}
